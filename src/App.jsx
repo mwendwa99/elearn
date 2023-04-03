8e1dc516c9967d3fd484204aace23f388c5dc1ae
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2023-04-03 09:09:41 +0300

    update loadscreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Suspense, lazy } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { CircularProgress, Box } from "@mui/material";
 // components
 import { Appbar } from "./components";
-import { Route, Routes } from "react-router-dom";
 
 // Define lazy-loaded components
 const Landing = lazy(() => import("./pages/Landing"));
@@ -11,7 +12,20 @@ const SignUp = lazy(() => import("./pages/SignUp"));
 
 function App() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <Box
+          sx={{
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      }
+    >
       <Appbar />
       <Routes>
         <Route exact path="/" element={<Landing />} />
